e1a283e4a37f7dfef49d7f46367c3e01d7cb1728
Emily Menezes <estudosmenezes@gmail.com>
2023-01-20 19:08:42 -0300

    feat: :sparkles: Estruturando navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Products from './pages/products';
 import './App.css';
 import Login from './pages/login';
 
@@ -8,6 +9,7 @@ function App() {
     <Switch>
       <Route path="/" exact component={ Login } />
       <Route path="/login" exact component={ Login } />
+      <Route path="/products" component={ Products } />
     </Switch>
   );
 }
---
+++ front-end/src/pages/products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Components/nav';
+
+export default class Products extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+      </div>
+    );
+  }
+}
