07815bae7ddf37716f3a34d3019195b97a4b439e
BrenoOPrado <breno.o.prado@hotmail.com>
2023-01-21 12:43:08 -0300

    tela de registro criada com  funcionamento parcial :turtle:

--- back-end/src/api/Routes/Routes.js
+++ back-end/src/api/Routes/Routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { insertLogin } = require('../Controllers/LoginController');
+const { insertLogin, registerUser } = require('../Controllers/LoginController');
 
 const routes = express.Router();
 
 routes.post('/login', insertLogin);
+routes.post('/register', registerUser);
 
 module.exports = routes;
