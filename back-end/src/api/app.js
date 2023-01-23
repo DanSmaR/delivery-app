671fffe69b1bd5576344987656b389c5500b8077
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-01-23 05:40:35 -0300

    ajustes backend e frontend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes/router');
 
 const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(router);
