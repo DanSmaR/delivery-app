d874b9d2f2d8aae42b7e02d9dc79aabc9874e3a4
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-01-23 05:40:00 -0300

    ajustes backend e frontend

--- front-end/src/pages/products.jsx
+++ front-end/src/pages/products.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../App.css';
 import Navbar from '../components/Navbar';
+import ProductsCards from '../components/ProductsCards';
 
 export default class Products extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar history={ this.props } />
+        <ProductsCards />
       </div>
     );
   }
