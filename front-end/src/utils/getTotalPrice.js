740f3cdda0467f75b200164e736038b584ff09c4
Danilo Martins <dansm86@outlook.com>
2023-01-24 17:59:09 -0300

    feat:checkout/add logica em getTotalPrice
    para verificar se existe lista de produtos

--- front-end/src/utils/getTotalPrice.js
+++ front-end/src/utils/getTotalPrice.js
@@ -1,4 +1,5 @@
 export default function getTotalPrice(selectedProductsList) {
+  if (!selectedProductsList || !selectedProductsList.length) return false;
   const totalPrice = selectedProductsList
     .reduce((acc, product) => acc + (product.quantity * product.price), 0);
   const roundedTotalPrice = Math.round(totalPrice * 100) / 100;
