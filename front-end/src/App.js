50e50e6013c00e957591cf69cca87c9537199dce
Danilo Martins <dansm86@outlook.com>
2023-01-24 18:51:02 -0300

    feat:checkout/add Checkout page no Router do App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/login';
 import Register from './pages/register';
 import Products from './pages/products';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" exact component={ Login } />
       <Route path="/register" exact component={ Register } />
       <Route path="/customer/products" component={ Products } />
+      <Route path="/customer/checkout" component={ Checkout } />
     </Switch>
   );
 }
