b53612075cc8b28fcc834894d65d1be6d084b49a
Danilo Martins <dansm86@outlook.com>
2023-01-25 01:02:29 -0300

    fix:checkout/refactored getTotalPrice function

--- front-end/src/utils/getTotalPrice.js
+++ front-end/src/utils/getTotalPrice.js
@@ -1,7 +1,6 @@
 export default function getTotalPrice(selectedProductsList) {
-  if (!selectedProductsList || !selectedProductsList.length) return false;
   const totalPrice = selectedProductsList
     .reduce((acc, product) => acc + (product.quantity * product.price), 0);
   const roundedTotalPrice = Math.round(totalPrice * 100) / 100;
-  return roundedTotalPrice;
+  return roundedTotalPrice.toFixed(2);
 }
