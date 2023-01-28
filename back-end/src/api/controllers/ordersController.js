72cbb1c3b8f0c44270c3906b1aa3b84617336b41
Danilo Martins <dansm86@outlook.com>
2023-01-27 21:46:50 -0300

    fix:orderUpdate/mudado status da resposta

--- back-end/src/api/controllers/ordersController.js
+++ back-end/src/api/controllers/ordersController.js
@@ -3,12 +3,11 @@ const ordersServices = require('../services/ordersServices');
 
 const updateStatus = async (req, res) => {
   const { id } = req.params;
-  const { status } = req.body;
+  const { status: orderStatus } = req.body;
   const { id: userId, role } = req.data;
-  await ordersServices.updateStatus(id, status);
-  const result = await getOrderById(id, userId, role);
-  const { message } = result;
-  return res.status(202).json(message);
+  await ordersServices.updateStatus(id, orderStatus);
+  const { status, message } = await getOrderById(id, userId, role);
+  return res.status(status).json({ status: message.status });
 };
 
 module.exports = {
