232a3c5bd5110249ec36e832bffbec8a8a3bc45b
Danilo Martins <dansm86@outlook.com>
2023-01-30 19:36:17 -0300

    feat:tests:front/created axios mock

---
+++ front-end/src/tests/__mocks__/axios.js
@@ -0,0 +1,7 @@
+export default {
+  get: jest.fn(() => Promise.resolve({ data: {} })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({})),
+  create: jest.fn(() => Promise.resolve({})),
+};
