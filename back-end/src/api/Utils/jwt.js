6dbc91489ef81cf2cebea1cc42b197c2063ac05c
Emily Menezes <estudosmenezes@gmail.com>
2023-01-30 21:17:42 -0300

    feat: testando rota /login

--- back-end/src/api/Utils/jwt.js
+++ back-end/src/api/Utils/jwt.js
@@ -5,9 +5,10 @@ const path = require('path');
 const JWT_EVALUATION_KEY = require('fs')
 .readFileSync(path.resolve(__dirname, '../../../jwt.evaluation.key'), { encoding: 'utf-8' });
 
-const TOKEN_SECRET_KEY = process.env.JWT_SECRET || JWT_EVALUATION_KEY;
+const TOKEN_SECRET_KEY = process.env.JWT_SECRET || JWT_EVALUATION_KEY || "secret_key";
 
 const generateToken = ({ id, name, email, role }) => {
+
   const payload = {
     id,
     name,
@@ -19,7 +20,6 @@ const generateToken = ({ id, name, email, role }) => {
     expiresIn: '100m',
     algorithm: 'HS256',
   };
-
   const token = jwt.sign(payload, TOKEN_SECRET_KEY, jwtConfig);
   return token;
 };
