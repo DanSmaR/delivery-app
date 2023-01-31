5a5ec7a3a3a85d46bf288457d3b8b7d528105719
Danilo Martins <dansm86@outlook.com>
2023-01-31 14:25:19 -0300

    feat:test/created constants

---
+++ front-end/src/tests/helpers/constants.js
@@ -0,0 +1,4 @@
+export const emailValid = 'user@email.com';
+export const emailInvalidList = ['', 'user', 'user@', 'user@mail', 'user@mail.c'];
+export const passwordValid = '123456';
+export const passwordInvalidList = ['', '12345'];
