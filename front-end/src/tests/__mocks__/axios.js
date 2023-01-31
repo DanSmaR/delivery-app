a4a2ed59dc70718d3abf1cc1cb4f89d528fafdb2
Danilo Martins <dansm86@outlook.com>
2023-01-31 19:56:40 -0300

    fix:test/refatorado mock do axios

--- front-end/src/tests/__mocks__/axios.js
+++ front-end/src/tests/__mocks__/axios.js
@@ -1,16 +1,8 @@
-import { emailValid, validToken, validUseName } from '../helpers/constants';
-
 export default {
-  get: jest.fn(() => Promise.resolve({
-    data: {
-      token: validToken,
-      id: 4,
-      email: validUseName,
-      name: emailValid,
-    },
+  create: jest.fn(() => ({
+    post: jest.fn(() => Promise.resolve({ data: {} })),
+    get: jest.fn(() => Promise.resolve({ data: {} })),
+    put: jest.fn(() => Promise.resolve({ data: {} })),
+    delete: jest.fn(() => Promise.resolve({ data: {} })),
   })),
-  post: jest.fn(() => Promise.resolve({ data: {} })),
-  put: jest.fn(() => Promise.resolve({ data: {} })),
-  delete: jest.fn(() => Promise.resolve({})),
-  create: jest.fn(() => Promise.resolve({})),
 };
