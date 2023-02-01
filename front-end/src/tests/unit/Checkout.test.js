823085025cd09ab97989e4eca6f488cdf2000ace
Danilo Martins <dansm86@outlook.com>
2023-02-01 20:18:17 -0300

    feat:test/Checkout-presença do formulário na pagin

--- front-end/src/tests/unit/Checkout.test.js
+++ front-end/src/tests/unit/Checkout.test.js
@@ -5,26 +5,52 @@ import App from '../../App';
 import renderWithRouter from '../helpers/renderWithRouter';
 // import Login from '../../pages/login';
 import {
-  sellerData,
+  cartStringfied,
+  sellerResponseData,
+  userStringfied,
 } from '../helpers/constants';
 import instance from '../../helpers/instance';
 
 describe('Testando a página de Checkout', () => {
-  const getAddressInput = () => screen.getByTestId('common_register__input-name');
-  const getNumberInput = () => screen.getByTestId('common_register__input-email');
-  const getSelectInput = () => screen.getByTestId('common_register__input-password');
+  const getAddressInput = () => screen.getByRole('textbox', { name: /Endereço/i });
+  const getNumberInput = () => screen.getByRole('spinbutton', { name: /Número/i });
+  const getSelectInput = () => screen.getByRole('combobox', { name: /P. Vendedora Responsável:/i });
   const getRemoveBtn = () => screen.getByRole('button', { name: /remover/i });
   const getOrderBtn = () => screen.getByRole('button', { name: /Finalizar Pedido/i });
 
-  const sellerResponseData = {
-    data: [sellerData],
-  };
-
-  const userStringfied = JSON.stringify(userDataResponse.data);
-
-  describe('', () => {
-    it('', async () => {
-
+  describe('Testando a existência do formulário de entrega e tabela de'
+    + ' produtos adicionados ao carrinho na página de produtos', () => {
+    beforeEach(() => {
+      instance.get.mockImplementationOnce(() => Promise.resolve(sellerResponseData));
+      localStorage.getItem.mockRestore();
+      localStorage.getItem.mockReturnValueOnce(cartStringfied);
+      localStorage.getItem.mockReturnValueOnce(userStringfied);
+      renderWithRouter(<App />, { initialEntries: ['/customer/checkout'] });
     });
+    it('Deve existir um formulário de Entrega com input de texto para'
+      + ' Endereço, um input de number para Número do Endereço e'
+      + 'um botão de Finalizar Pedido desabilitado', async () => {
+      expect(getAddressInput()).toBeInTheDocument();
+      expect(getNumberInput()).toBeInTheDocument();
+      expect(getSelectInput()).toBeInTheDocument();
+      expect(screen.getByRole('heading', { level: 2, name: /Detalhes e Endereço para Entrega/i }));
+    });
+  });
+
+  beforeAll(() => {
+    jest.spyOn(Storage.prototype, 'setItem');
+    jest.spyOn(Storage.prototype, 'getItem');
+    Storage.prototype.setItem = jest.fn();
+    Storage.prototype.getItem = jest.fn();
+  });
+
+  beforeEach(() => {
+    localStorage.getItem.mockReturnValueOnce(null);
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+    Storage.prototype.setItem.mockClear();
+    Storage.prototype.getItem.mockClear();
   });
 });
