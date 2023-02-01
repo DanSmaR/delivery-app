0ba0b543bcf88d041d0c9733941c44236d04ab71
Danilo Martins <dansm86@outlook.com>
2023-02-01 01:25:23 -0300

    fix:test/mudado implementação mock axios

--- front-end/src/tests/__mocks__/axios.js
+++ front-end/src/tests/__mocks__/axios.js
@@ -1,8 +1,8 @@
 export default {
   create: jest.fn(() => ({
-    post: jest.fn(() => Promise.resolve({ data: {} })),
-    get: jest.fn(() => Promise.resolve({ data: {} })),
-    put: jest.fn(() => Promise.resolve({ data: {} })),
-    delete: jest.fn(() => Promise.resolve({ data: {} })),
+    post: jest.fn(),
+    get: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
   })),
 };
